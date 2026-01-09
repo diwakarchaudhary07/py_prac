2fb4a7c47cfb4c2b2e83efccfdfdab34584c0cf5
Diwakar_Chaudhary_07 <boyroyal4853@gmail.com>
2026-01-09 21:42:13 +0530

    add this code

--- lists/frstday.py
+++ lists/frstday.py
@@ -1,1 +1,49 @@
-print("hello world")
+# print("hello world")
+# my_dict =  {
+                    # "name": "Ujwal",
+                    # "age": 24,
+                    # "city": "Bangalore",
+                    # "roll_no": 101
+# }
+# 
+# 
+# print(my_dict)
+# print(my_dict.keys())  ## accessing all keys in the dictionary
+
+
+# diwakar_dictionary =  {
+                    # "name": "Ujwal",
+                    # "age": 24,
+                    # "city": "Bangalore",
+                    # "roll_no": 101
+# }
+# 
+# 
+# diwakar_dictionary.update({"roll_no": 251})
+# print(diwakar_dictionary)
+
+# 
+
+
+
+
+
+
+# pop()
+
+
+
+
+
+
+diwakar_dictionary =  {
+                    "name": "Ujwal",
+                    "age": 24,
+                    "city": "Bangalore",
+                    "roll_no": 101
+}
+
+
+diwakar_dictionary.pop("city")
+print(diwakar_dictionary)
+
